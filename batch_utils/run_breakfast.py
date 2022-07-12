e15285b43c1d8a72c99925e98b5c8649dba937a9
Naoki-Wake <nawake@microsoft.com>
2022-07-13 08:29:18 +0900

    fix

--- batch_utils/run_breakfast.py
+++ batch_utils/run_breakfast.py
@@ -13,19 +13,22 @@ if __name__ == '__main__':
     parser.add_argument('--config', default='configs/recognition/arr_tsm2022/tsm_r50_1x1x8_50e_breakfast_rgb.py', type=str)
     parser.add_argument('--load-from', default='/lfovision_sthv2_breakfast/pretrained_models/tsm_r50_256h_1x1x8_50e_sthv2_rgb_20210816-032aa4da.pth', type=str)
     parser.add_argument('--work-dir-root', default='/lfovision_log/tsm_learningrate/', type=str)
-    parser.add_argument('--work-dir-name', default='debug', type=str)
+    parser.add_argument('--work-dir-name', default='', type=str)
     parser.add_argument('--train-file-dir', default='/lfovision_sthv2_breakfast/annotations/with_pseudo_largedatanum/', type=str)
     parser.add_argument('--dir-videos-root', default='/lfovision_sthv2_breakfast/', type=str)
     parser.add_argument('--videos-per-gpu', default=30, type=int)
     parser.add_argument('--workers-per-gpu', default=4, type=int)
     parser.add_argument('--lr', default=0.0075, type=float)
     parser.add_argument('--weight_decay', default=0.0005, type=float)
+    parser.add_argument('--momentum', default=0.9, type=float)
 
     args = parser.parse_args()
     # ----settings-----
     fp_config_out = '/tmp/config.py'
+    if args.work_dir_name == '':
+        work_dir_name = "lr_{}_wd_{}_momentum_{}".format(args.lr, args.weight_decay, args.momentum)
     cfg = Config.fromfile(osp.join(args.dir_root,args.config))
-    cfg_options = {'work_dir': osp.join(args.work_dir_root,args.work_dir_name),
+    cfg_options = {'work_dir': osp.join(args.work_dir_root,work_dir_name),
                    'data.train.ann_file': osp.join(args.train_file_dir, 'breakfast_train_list_videos.txt'),
                    'data.val.ann_file': osp.join(args.train_file_dir, 'breakfast_val_list_videos.txt'),
                    'data.test.ann_file': osp.join(args.train_file_dir, 'breakfast_test_list_videos.txt'),
@@ -41,7 +44,8 @@ if __name__ == '__main__':
                    'data.videos_per_gpu': args.videos_per_gpu,
                    'data.workers_per_gpu': args.workers_per_gpu,
                    'optimizer.lr': args.lr,
-                   'optimizer.weight_decay': args.weight_decay,}
+                   'optimizer.weight_decay': args.weight_decay,
+                   'optimizer.momentum': args.momentum,}
     cfg.merge_from_dict(cfg_options)
     cfg.dump(fp_config_out)
 
@@ -51,7 +55,7 @@ if __name__ == '__main__':
     print(train_command)
     #subprocess.run([train_command], shell=True)
 #
-    test_command = str(osp.join(args.dir_root, "/tools/test_several.py")) + " " + fp_config_out + " " + osp.join(
+    test_command = "python " + str(osp.join(args.dir_root, "tools/test_several.py")) + " " + fp_config_out + " " + osp.join(
         osp.join(args.work_dir_root,args.work_dir_name),
         'epoch_0.pth') + " --eval top_k_accuracy mean_class_accuracy --out " + osp.join(
         osp.join(args.work_dir_root,args.work_dir_name),
--- configs/recognition/arr_tsm2022/tsm_r50_1x1x8_50e_breakfast_rgb.py
+++ configs/recognition/arr_tsm2022/tsm_r50_1x1x8_50e_breakfast_rgb.py
@@ -1,5 +1,7 @@
 from linecache import lazycache
 
+from cv2 import moments
+
 
 _base_ = [
     '../../_base_/models/breakfast_nonaddlayer_r50.py', '../../_base_/schedules/sgd_tsm_50e.py',
@@ -92,7 +94,8 @@ evaluation = dict(
 # optimizer
 optimizer = dict(
     lr=0.0075,  # this lr is used for 8 gpus
-    weight_decay=0.0005)
+    weight_decay=0.0005,
+    momentum=0.9)
 
 # runtime settings
 work_dir = './work_dirs/experiment20220628_with_pseudo_largedatanum_onlyheader/'
