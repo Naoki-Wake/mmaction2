d487eee59b53907a1f76387573224e52b8f621d8
Naoki-Wake <nawake@microsoft.com>
2022-07-13 08:35:56 +0900

    fix

--- batch_utils/run_breakfast.py
+++ batch_utils/run_breakfast.py
@@ -27,7 +27,8 @@ if __name__ == '__main__':
     fp_config_out = '/tmp/config.py'
     if args.work_dir_name == '':
         work_dir_name = "lr_{}_wd_{}_momentum_{}".format(args.lr, args.weight_decay, args.momentum)
-    #print('work_dir_name:', work_dir_name)
+    work_dir_name = args.work_dir_name
+    print('work_dir_name:', work_dir_name)
     cfg = Config.fromfile(osp.join(args.dir_root,args.config))
     cfg_options = {'work_dir': osp.join(args.work_dir_root, work_dir_name),
                    'data.train.ann_file': osp.join(args.train_file_dir, 'breakfast_train_list_videos.txt'),
@@ -56,8 +57,8 @@ if __name__ == '__main__':
     #subprocess.run([train_command], shell=True)
 #
     test_command = "python " + str(osp.join(args.dir_root, "tools/test_several.py")) + " " + fp_config_out + " " + osp.join(
-        osp.join(args.work_dir_root,args.work_dir_name),
+        osp.join(args.work_dir_root,work_dir_name),
         'epoch_0.pth') + " --eval top_k_accuracy mean_class_accuracy --out " + osp.join(
-        osp.join(args.work_dir_root,args.work_dir_name),
+        osp.join(args.work_dir_root,work_dir_name),
         'test_result.json')
     print(test_command)
