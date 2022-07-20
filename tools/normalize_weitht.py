006d7ca8075e6951562172c89a9e7423a68f9d3a
Naoki Wake <naoki.wake@microsoft.com>
2022-07-19 21:02:28 -0700

    add utils

---
+++ tools/normalize_weitht.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+from mmcv import Config
+from mmaction.apis import inference_recognizer, init_recognizer
+import os.path as osp
+
+
+#----settings-----
+checkpoint = '/lfovision_log/tsm_learningrate/lr_0.01_wd_0.0005_momentum_0.9_bn_true_cosine/epoch_50.pth'
+fp_config = '/lfovision_log/tsm_learningrate/lr_0.01_wd_0.0005_momentum_0.9_bn_true_cosine/config.py'
+label = ' /lfovision_sthv2_breakfast/annotations/something-something-v2-labels.txt'
+# assign the desired device.
+device = torch.device('cuda:0')
+cfg = Config.fromfile(fp_config)
+cfg_options = {}
+cfg.merge_from_dict(cfg_options)
+# build the recognizer from a config file and checkpoint file/url
+model = init_recognizer(fp_config, checkpoint, device=device)
+weight = model.cls_head.fc_cls.weight.cpu().detach().numpy()
+bias = model.cls_head.fc_cls.bias.cpu().detach().numpy()
+
+# modify the weight and bias
+for i in range(len(bias)):
+    model.cls_head.fc_cls.bias[i] = 0.0
+
+# normalize each row by L2 norm
+for row in range(len(bias)):
+    weight[row,:] = weight[row,:]/np.linalg.norm(weight[row,:], ord=2)
+    weight[row,:] = weight[row,:]/np.sum(weight[row,:])
+
+for i in range(weight.shape[0]):
+    for j in range(weight.shape[1]):
+        model.cls_head.fc_cls.weight[i][j] = float(weight[i,j])
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+# show the weight and bias
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.imshow(weight, cmap=cm.jet)
+ax.set_title('weight')
+plt.savefig(osp.join(outdir, 'weight.png'))
+
